c0820c3906ab326b9744b4e43a5c86f65efd1ed4
Naveen Kumar Gupta <124282752+nvikumar@users.noreply.github.com>
2026-01-04 23:35:58 +0530

    added second method vector reversal implementation
    Commented out previous implementations of vector reversal.

--- VerilogLanguage/02 Vectors/07_vector_reversal.v
+++ VerilogLanguage/02 Vectors/07_vector_reversal.v
@@ -2,6 +2,13 @@ module top_module(
     input [7:0] in,
     output [7:0] out
 );
-    assign { out } = { in[0],in[1],in[2],in[3],in[4],in[5],in[6],in[7]};
+    // m = 1 ---------------------------------
+    // assign { out } = { in[0],in[1],in[2],in[3],in[4],in[5],in[6],in[7]};
+
+    // m = 2 ---------------------------------
+    // assign {out[0],out[1],out[2],out[3],out[4],out[5],out[6],out[7]} = in;
+
+    // m = 3 ---------------------------------
+    // using loop 
 
 endmodule
