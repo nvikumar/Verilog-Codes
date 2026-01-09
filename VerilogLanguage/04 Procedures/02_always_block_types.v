bf3d47f285413b0289cbb43987fb037bf3b31722
Naveen Kumar Gupta <124282752+nvikumar@users.noreply.github.com>
2026-01-09 22:33:55 +0530

    Add always block types in Verilog module

---
+++ VerilogLanguage/04 Procedures/02_always_block_types.v
@@ -0,0 +1,16 @@
+// synthesis verilog_input_version verilog_2001
+module top_module(
+    input clk,
+    input a,
+    input b,
+    output wire out_assign,
+    output reg out_always_comb,
+    output reg out_always_ff   );
+    
+    assign out_assign = a ^ b;
+    always @(*) out_always_comb = a ^ b;
+  
+    // clocked always block produces a different circuit from the other two: There is a flip-flop so the output is delayed.
+    always @(posedge clk) out_always_ff = a ^ b;
+
+endmodule
