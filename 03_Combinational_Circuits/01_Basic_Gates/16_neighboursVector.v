179f2d90da85fcf20f088f37b371bc6030c944b4
Naveen G. <124282752+nvikumar@users.noreply.github.com>
2026-01-17 19:34:06 +0530

    Refactor output assignments and enhance comments
    Refactored output assignments for both and any conditions using bitwise operations. Added comments for clarity on the calculations and vector manipulations.

--- 03_Combinational_Circuits/01_Basic_Gates/16_neighboursVector.v
+++ 03_Combinational_Circuits/01_Basic_Gates/16_neighboursVector.v
@@ -3,9 +3,36 @@ module top_module(
     output [2:0] out_both,
     output [3:1] out_any,
     output [3:0] out_different );
-    
-    assign out_both = in[2:0] & {in[3:1]};
-    assign out_any = in[2:0] | {in[3:1]};
+
+    // Use bitwise operators and part-select to do the entire calculation in one line of code
+	// in[3:1] is this vector:   					 in[3]  in[2]  in[1]
+	// in[2:0] is this vector:   					 in[2]  in[1]  in[0]
+	// Bitwise-OR produces a 3 bit vector.			   |      |      |
+	// Assign this 3-bit result to out_any[3:1]:	o_a[3] o_a[2] o_a[1]
+
+	// Thus, each output bit is the OR of the input bit and its neighbour to the right:
+	// e.g., out_any[1] = in[1] | in[0];	
+	// Notice how this works even for long vectors.
+    assign out_both = in[2:0] & in[3:1];
+    assign out_any = in[2:0] | in[3:1];
+
+    // XOR 'in' with a vector that is 'in' rotated to the right by 1 position: {in[0], in[3:1]}
+	// The rotation is accomplished by using part selects[] and the concatenation operator{}.
     assign out_different = in ^ {in[0], in[3:1]};
+
+
+
+    // m = 2 : using loop
+    /*
+    always @(*)	begin
+		out_different[3] = in[3] ^ in[0];
+        
+		for(i=0; i<3; i++) begin
+			out_both[i] = in[i] & in[i+1];
+			out_any[i+1] = in[i] | in[i+1];
+			out_different[i] = in[i] ^ in[i+1];
+		end
+	end
+    */
 
 endmodule
