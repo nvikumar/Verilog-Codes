6ec73c938fb23438be881643bdc39756117ec2ed
Naveen G. <124282752+nvikumar@users.noreply.github.com>
2026-01-29 14:17:26 +0530

    Update 11_muxAndDff_1.v

--- 04_sequential_Circuits/01_latches_FlipFlop/11_muxAndDff_1.v
+++ 04_sequential_Circuits/01_latches_FlipFlop/11_muxAndDff_1.v
@@ -16,4 +16,14 @@ module top_module (
         Q <= d;
   */
 
+	// m = 3: if else statement also creates a mux
+	/*
+	always @(posedge clk) begin
+     if(L)
+        Q <= r_in;
+     else
+        Q <= q_in;
+  	end
+  */
+
 endmodule
